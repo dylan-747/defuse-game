48ab8bb95770746e0c9d5aace35acfdaaf96ac55
Dylan Galloway <dylangalloway@Dylans-Air.Home>
2025-05-26 14:54:30 +0100

    Add footer signature with mailto link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+// src/App.js
 import React from "react";
 import DefuseGame from "./DefuseGame";
+import "./App.css";
 
 export default function App() {
-  return <DefuseGame />;
+  return (
+    <div className="app-wrapper">
+      <DefuseGame />
+      <footer className="app-footer">
+        Built by{' '}
+        <a href="mailto:jgdylan@icloud.com">
+          Dylan Galloway
+        </a>
+      </footer>
+    </div>
+  );
 }
